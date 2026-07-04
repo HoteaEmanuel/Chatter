dff003089a13693fa0dab5ab68a12055513ecc02
Hotea Emanuel <emanuelhotea1@gmail.com>
2026-07-04 17:01:29 +0300

    feat:Email idx migration

---
+++ src/common/database/db-migration.service.ts
@@ -0,0 +1,25 @@
+import { ConfigService } from '@nestjs/config';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { config, database, up } from 'migrate-mongo';
+@Injectable()
+export class DBMigrationService implements OnModuleInit {
+  private readonly dbMigrationConfig: Partial<config.Config>;
+
+  constructor(private readonly configService: ConfigService) {
+    this.dbMigrationConfig = {
+      mongodb: {
+        databaseName: this.configService.getOrThrow('DB_NAME'),
+        url: this.configService.getOrThrow('MONGODB_URI'),
+      },
+      migrationsDir: `${__dirname}/../../migrations`,
+      changelogCollectionName: 'changelog',
+      migrationFileExtension: '.js',
+    };
+  }
+
+  async onModuleInit() {
+    config.set(this.dbMigrationConfig);
+    const { db, client } = await database.connect();
+    await up(db, client);
+  }
+}
